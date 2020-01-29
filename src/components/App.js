71663d965a11442004a13dcd7ea09ddf6c9a6805
Alice Won <alicewon4@gmail.com>
2020-01-28 17:22:49 -0800

    Done.

--- src/components/App.js
+++ src/components/App.js
@@ -8,11 +8,37 @@ class App extends React.Component {
     super()
 
     this.state = {
-      pets: [],
-      filters: {
+        pets: [],
+        filters: {
         type: 'all'
       }
     }
+  }
+
+
+  fetchPets = () => {
+    let endpoint = '/api/pets'
+
+    //if selection is anything other than "all", then find the type that was selected by adding it to the end of the endpoint call
+    if (this.state.filters.type !== 'all') {
+      endpoint += `?type=${this.state.filters.type}`
+    }
+
+    fetch(endpoint)
+    .then(res => res.json())
+    .then(pets => this.setState({ pets: pets }))
+  }
+
+  //selects type that was selected to show on change
+  onChangeType = ({ target: {value} }) => {
+    this.setState({filters: {...this.state.filters, type: value}})
+  }
+
+  onAdoptPet = petId => {
+    const pets = this.state.pets.map(p => {
+      return p.id === petId ? {...p, isAdopted: true} : p;
+    })
+    this.setState({ pets: pets })
   }
 
   render() {
@@ -24,10 +50,15 @@ class App extends React.Component {
         <div className="ui container">
           <div className="ui grid">
             <div className="four wide column">
-              <Filters />
+              <Filters 
+                onChangeType={this.onChangeType}
+                onFindPetsClick={this.fetchPets}
+              />
             </div>
             <div className="twelve wide column">
-              <PetBrowser />
+              <PetBrowser 
+              pets={this.state.pets}
+              onAdoptPet={this.onAdoptPet}/>
             </div>
           </div>
         </div>
--- src/components/Filters.js
+++ src/components/Filters.js
@@ -1,21 +1,31 @@
 import React from 'react'
 
 class Filters extends React.Component {
+
+
   render() {
     return (
       <div className="ui form">
         <h3>Animal type</h3>
         <div className="field">
-          <select name="type" id="type">
+          <select name="type" id="type"
+          onChange={this.props.onChangeType} >
+{/* refers to onChangeType function in App.js */}
+
             <option value="all">All</option>
             <option value="cat">Cats</option>
             <option value="dog">Dogs</option>
             <option value="micropig">Micropigs</option>
           </select>
+          
         </div>
 
         <div className="field">
-          <button className="ui secondary button">Find pets</button>
+          <button 
+          onClick={this.props.onFindPetsClick}
+          // refers to onFindPetsClick function in App.js
+          
+          className="ui secondary button">Find pets</button>
         </div>
       </div>
     )
